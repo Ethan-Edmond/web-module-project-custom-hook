1cb91cdbc5235601c9bf37b7feaec32b2fa9903e
Ethan-Edmond <eedmond@uccs.edu>
2021-06-02 17:17:34 -0600

    Moved App back to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import Charts from "./components/Charts";
+import Navbar from "./components/Navbar";
+
+import useDarkMode from "./hooks/useDarkMode";
+import useCoinData from "./hooks/useCoin";
 
 import "./styles.scss";
+
+
+const App = () => {
+  const [coinData] = useCoinData([]);
+  const [darkMode, setDarkMode] = useDarkMode(false);
+
+  return (
+    <div className={darkMode ? "dark-mode App" : "App"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Charts darkMode={darkMode} coinData={coinData} />
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
